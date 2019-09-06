13389ae989e8e7ba84cb5319a5585a04c9fb7cf1
Surendra C <suredra_advanz@bitbucket.org>
2019-09-06 18:01:15 +0530

    local changes

--- src/app/dashboard/baby-cns/baby-cns.component.ts
+++ src/app/dashboard/baby-cns/baby-cns.component.ts
@@ -33,10 +33,10 @@ export class BabyCnsComponent implements OnInit {
   temp_study_id = 0;
 
   login_hospital: any = {};
-  content:any;
+  content: any;
   public dataServiceObj;
   public readingDataObj;
-  isEditClicked=false;
+  isEditClicked = false;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -46,7 +46,7 @@ export class BabyCnsComponent implements OnInit {
     private modalService: NgbModal,
     private commonAsyn: Common,
     private dataService: DataService,
-    public readingDataService:ReadingDataService
+    public readingDataService: ReadingDataService
   ) {
     this.dataServiceObj = dataService.getOption();
   }
@@ -54,24 +54,23 @@ export class BabyCnsComponent implements OnInit {
   ngOnInit() {
     const vim = this;
     vim.dataServiceObj = vim.dataService.getOption();
-    vim.readingDataObj=vim.readingDataService.getReadingFormData('baby_cns') ;
+    vim.readingDataObj = vim.readingDataService.getReadingFormData('baby_cns');
     vim.login_hospital = JSON.parse(
       localStorage.getItem("login_hospital"));
     vim.id = vim.dataServiceObj.study_id;
     vim.createForm(vim.dataServiceObj.study_id);
     vim.temp_study_id = vim.id;
-    if(vim.readingDataObj!=undefined){
-      vim.getMedicalRecordNumber=vim.dataServiceObj.baby_medical_record_number;
+    if (vim.readingDataObj != undefined) {
+      vim.getMedicalRecordNumber = vim.dataServiceObj.baby_medical_record_number;
       vim.getReadingFormData(this.readingDataObj);
-   }
-     else{
-     if ( vim.dataServiceObj.study_id != undefined) {
-      vim.getMedicalRecordNumber=vim.dataServiceObj.baby_medical_record_number;
-      vim.get_baby_cns(vim.dataServiceObj.study_id, vim.login_hospital['id'], vim.page, vim.readingDataService.reading);
     }
-    vim.onChanges();
-}
-    
+    else {
+      if (vim.dataServiceObj.study_id != undefined) {
+        vim.getMedicalRecordNumber = vim.dataServiceObj.baby_medical_record_number;
+        vim.get_baby_cns(vim.dataServiceObj.study_id, vim.login_hospital['id'], vim.page, vim.readingDataService.reading);
+      }
+      vim.onChanges();
+    }
 
     this.subscription = this.common_api.getMedicalRecordNumber().subscribe(message => {
       this.getMedicalRecordNumber = message.text;
@@ -113,7 +112,7 @@ export class BabyCnsComponent implements OnInit {
     if (!_.isEmpty(obj)) {
       this.isEdit = true;
       this.updateForm(obj);
-      this.isEditClicked=true;
+      this.isEditClicked = true;
     } else {
       this.isEdit = true;
       this.createForm(this.id);
@@ -126,140 +125,21 @@ export class BabyCnsComponent implements OnInit {
     if (vim.babyCnsForm.invalid) {
       return;
     }
-   // vim.commonAsyn.showLoader();
     vim.babyCnsForm.value["tab_name"] = "baby_cns_add";
-    // const newUser = vim.common_api.baby_cns_add(vim.babyCnsForm.value);
-    // newUser.subscribe(
-    //   response => {
-    //     vim.reset();
-    //     vim.success(response, "cnsFormSubmit");
-    //   },
-    //   error => {
-    //     console.error("errro", error);
-    //   }
-    // );
     this.babyCnsForm.value["reading"] = localStorage.getItem('reading');
     vim.goToNextReadingForm();
   }
-  /**
-   *
-   * @param response
-   * @param api_type
-   * @method: success
-   * @purpose :-  it is a common helper
-   */
 
   success(response, api_type) {
     const vim = this;
-    if (api_type == "cnsFormSubmit") {
-      if (vim.isSuccess(response)) {
-        vim.toastr.success(
-          "",
-          "Information Updated succesfully"
-        );
-        vim.responseArray = [];
-        this.page = 1;
-        vim.dataServiceObj = vim.dataService.getOption();
-        vim.get_baby_cns(vim.dataServiceObj.study_id, vim.login_hospital['id'], vim.page, vim.readingDataService.reading);
-      } else {
-        if (vim.isAlreadyExist(response)) {
-          vim.toastr.warning("Already Exist!!", response["message"]);
-        } else {
-          vim.errorToasty(response);
-        }
-      }
-    } else if (api_type == "get_baby_cns") {
-      if (vim.isSuccess(response)) {
-        if (this.page == 1) {
-          vim.responseArray = [];
-          vim.responseArray = response["response"];
-          vim.isEdit=false;
-        } else {
-          if (response["status"] == 404) {
-          }
-          else if (response["response"].length > 0) {
-            vim.temp_study_id = response["response"][0].study_id;
-            if (vim.temp_study_id == vim.id) {
-            } else {
-              vim.responseArray = [];
-            }
-
-            for (var i = 0; i < response["response"].length; i++) {
-              vim.responseArray.push(response["response"][i]);
-              vim.temp_study_id = vim.id;
-            }
-          }
-        }
-        vim.commonAsyn.isHide();
-      } else {
-        vim.responseArray = [];
-        vim.commonAsyn.isHide();
-        if (vim.isAlreadyExist(response)) {
-        } else {
-          // vim.errorToasty(response);
-        }
-      }
+    if (api_type == "get_baby_cns") {
+      vim.responseArray = [];
+      vim.responseArray = response["response"];
+      vim.isEdit = false;
+      vim.commonAsyn.isHide();
     }
   }
 
-  /**
-   *
-   * @param error
-   * @param api_type
-   * @purpose :-  This is error handler method is called.
-   * @method: errorHandler
-   */
-  errorHandler(error, api_type) {
-    const vim = this;
-    if (api_type == "cnsFormSubmit") {
-      vim.errorToasty(error);
-    }
-  }
-
-  /**
-   *
-   * @param response
-   * @method: it is a common herlper for check the status is 200 or not
-   */
-  isSuccess(response) {
-    const vim = this;
-    if (
-      response.hasOwnProperty("status") &&
-      response["status"] === vim.success_status
-    ) {
-      return true;
-    } else if (response["status"] === 404) {
-      return true;
-    }
-    return false;
-  }
-  /**
-   *
-   * @param response
-   * @method :- isAlreadyExist
-   * @purpose :- check if User Already Exist.
-   */
-  isAlreadyExist(response) {
-    const vim = this;
-    if (
-      response.hasOwnProperty("status") &&
-      response["status"] === vim.already_exist_status
-    ) {
-      return true;
-    }
-    return false;
-  }
-  /**
-   * @method :- errorToasty
-   */
-  errorToasty(error) {
-    const vim = this;
-    if (error.hasOwnProperty("message")) {
-      vim.toastr.error("Error!", error["message"]);
-    } else {
-      vim.toastr.error("Error!", "Somethink wrong!!!..");
-    }
-  }
 
   get_baby_cns(id, hospital_id, page, reading) {
     const vim = this;
@@ -274,25 +154,25 @@ export class BabyCnsComponent implements OnInit {
       response => {
         vim.success(response, "get_baby_cns");
         vim.isEdit = false;
-        vim.isEditClicked=false;
+        vim.isEditClicked = false;
       },
       error => {
         console.error("errro", error);
       }
     );
   }
-  getReadingFormData(formData){
-    this.responseArray[0]=formData;
+  getReadingFormData(formData) {
+    this.responseArray[0] = formData;
     this.updateForm(this.responseArray[0]);
-    this.isEdit=true;
+    this.isEdit = true;
   }
 
-  saveReadingFormData(formData){
-    this.readingDataService.setReadingFormData('baby_cns',formData);
+  saveReadingFormData(formData) {
+    this.readingDataService.setReadingFormData('baby_cns', formData);
   }
 
-  goToNextReadingForm(){
-    let vim=this;
+  goToNextReadingForm() {
+    let vim = this;
     vim.saveReadingFormData(vim.babyCnsForm['value']);
     vim.readingDataService.setComponentFlag('baby-git')
     vim.readingDataService.setActiveTab("baby-gi-tract");
@@ -301,16 +181,16 @@ export class BabyCnsComponent implements OnInit {
 
   onChanges(): void {
     this.babyCnsForm.statusChanges.subscribe(val => {
-      if(val==='INVALID'){
-        this.readingDataService.setFormValidationStatus('baby_cns',false)
-          if(this.readingDataObj!=undefined){
-            this.babyCnsForm.value["reading"] = localStorage.getItem('reading');
-            this.saveReadingFormData(this.babyCnsForm['value']);
-          }
+      if (val === 'INVALID') {
+        this.readingDataService.setFormValidationStatus('baby_cns', false)
+        if (this.readingDataObj != undefined) {
+          this.babyCnsForm.value["reading"] = localStorage.getItem('reading');
+          this.saveReadingFormData(this.babyCnsForm['value']);
+        }
       }
-      else{
-        this.readingDataService.setFormValidationStatus('baby_cns',true)
-        if(this.readingDataObj!=undefined){
+      else {
+        this.readingDataService.setFormValidationStatus('baby_cns', true)
+        if (this.readingDataObj != undefined) {
           this.babyCnsForm.value["reading"] = localStorage.getItem('reading');
           this.saveReadingFormData(this.babyCnsForm['value']);
         }
@@ -318,26 +198,26 @@ export class BabyCnsComponent implements OnInit {
     });
   }
 
-  updateCNSForm(){
-    if(!this.babyCnsForm.valid){
-      return ;
+  updateCNSForm() {
+    if (!this.babyCnsForm.valid) {
+      return;
     }
-    else{
-      this.common_api.updateFormData('patient/update/baby_cns/',this.id,this.readingDataService.reading,this.babyCnsForm['value']).subscribe(result=>{
-          if(result['status']!=200){
-              this.toastr.error('Error','Some error occured.Please check');
-          }
-          else{
-            this.updateSuccessResponse(result);
-          }
+    else {
+      this.common_api.updateFormData('patient/update/baby_cns/', this.id, this.readingDataService.reading, this.babyCnsForm['value']).subscribe(result => {
+        if (result['status'] != 200) {
+          this.toastr.error('Error', 'Some error occured.Please check');
+        }
+        else {
+          this.updateSuccessResponse(result);
+        }
       })
     }
   }
 
-  updateSuccessResponse(result){
-    this.toastr.success('','Data Updated Successfully');
+  updateSuccessResponse(result) {
+    this.toastr.success('', 'Data Updated Successfully');
     this.get_baby_cns(this.dataServiceObj.study_id, this.login_hospital['id'], this.page, this.readingDataService.reading);
-    this.isEditClicked=false;
-  //  this.saveReadingFormData(undefined);
+    this.isEditClicked = false;
+    //  this.saveReadingFormData(undefined);
   }
 }
